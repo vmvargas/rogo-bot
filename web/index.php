356a6d9ed5d2a6dae2b8ff60f5ff6d814ae74ed2
vmvargas <victor07vargas@gmail.com>
2017-10-15 19:27:45 -0400

    test 3 - testing with basic info

--- web/index.php
+++ web/index.php
@@ -1,38 +1,12 @@
 <?php
-//grab twilio user number and msg content
-$user_number = $_POST['From'];
-$user_msg = $_POST['Body'];
+$number = $_POST['From'];
+$body = $_POST['Body'];
 header('Content-Type: text/xml');
-
-//generate API Request
-$WA_request = "https://api.wolframalpha.com/v2/result?";
-$WA_query = array(
-    'i' => $user_msg,
-    'appid' => 'DEMO'
-);
-
-// generate cURL session
-$url = $WA_request.$WA_query;
-// create a new cURL resource
-$ch = curl_init();
-// set URL and other appropriate options
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 4);
-// grab URL and pass it to the browser
-$json = curl_exec($ch);
-//validate response
-if(!$json) {
-    echo curl_error($ch);
-}
-// close cURL resource, and free up system resources
-curl_close($ch);
-print_r(json_decode($json));
-
-// Send SMS Response:
 ?>
+
 <Response>
     <Message>
-        <?php json_decode($json)?>
+        Hello <?php echo $number ?>.
+        You said <?php echo $body ?>
     </Message>
 </Response>
