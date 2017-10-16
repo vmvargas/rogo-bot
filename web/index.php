83f7ea1233d23a78586a4eea9f224e0ff7a94343
vmvargas <victor07vargas@gmail.com>
2017-10-15 20:21:57 -0400

    test 4

--- web/index.php
+++ web/index.php
@@ -1,12 +1,43 @@
 <?php
-$number = $_POST['From'];
-$body = $_POST['Body'];
+//grab twilio user number and msg content
+$user_number = $_POST['From'];
+$user_msg = $_POST['Body'];
 header('Content-Type: text/xml');
-?>
 
+//generate API Request
+$WA_request = "https://api.wolframalpha.com/v2/result?";
+$WA_query = array(
+    'i' => "is a tomato a fruit?",
+    'appid' => 'DEMO'
+);
+
+// generate cURL session
+$url = $WA_request.$WA_query;
+// create a new cURL resource
+$ch = curl_init();
+// set URL and other appropriate options
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 4);
+// grab URL and pass it to the browser
+$json = curl_exec($ch);
+//validate response
+if(!$json) {
+    echo curl_error($ch);
+}
+// close cURL resource, and free up system resources
+curl_close($ch);
+
+//print variables
+echo '$user_msg: '. $user_msg;
+echo '$url: '. $url;
+echo '$json: '. $json;
+print_r(json_decode($json));
+
+// Send SMS Response:
+?>
 <Response>
     <Message>
-        Hello <?php echo $number ?>.
-        You said <?php echo $body ?>
+        <?php json_decode($json)?>
     </Message>
 </Response>
